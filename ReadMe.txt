6fdb52cdce71bc3ac58eac48eb1be2f361ef9583
Nissara47 <nissara.nnd@gmail.com>
2023-02-27 23:18:06 +0700

    init

---
+++ ReadMe.txt
@@ -0,0 +1,7 @@
+**โปรดระวังหากมี database ที่ชื่อ project อยู่แล้วในเครื่อง ข้อมูลจะถูกลบ
+***เว็บไซต์จะทำงานได้ดีที่สุดในหน้าจอขนาดประมาณ 1600x900 pixel
+
+database และ website จำเป็นต้องใช้งานผ่าน XAMPP โดย start Apache และ MySQL
+1. import file "SQL.sql" ลงใน phpMyAdmin ไฟล์จะสร้าง database ที่ชื่อว่า project
+2. ย้ายโฟลเดอร์ Website ลงใน 'C:\xampp\htdocs\...'
+3. เริ่มต้นใช้งาน โดยเข้าใช้งานผ่าน URL 'localhost\Website\Start.php' ใน browser ของท่าน
---
+++ Website/Start.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="ReadAWrite/CSS/Start.css">
+    </head>
+    <body>
+        <div class="myl">
+            <img src="ReadAWrite/image_nav/RAW-Login_v3@2x.png">
+        </div>
+        <div class="myr">
+            <img src="ReadAWrite/image_nav/readawrite-logo-svg.svg">
+            <p>
+                นิยาย นิยายออนไลน์<br>แหล่งกบดานใหม่<br>
+                สำหรับผู้ที่รักการอ่านและเขียน<br><br>
+                <a href="ReadAWrite/unknown_homepage.php">เข้าสู่หน้าเว็บไซต์</a>
+            </p>
+        </div>
+    </body>
+</html>
